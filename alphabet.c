77f85a08b80793b65148fe7662eaae4d64f193ab
Ramyaamirthalingam <ramya12.eng@gmail.com>
2017-06-21 14:58:30 +0530

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char ch;
+printf("enter your input");
+Scanf("%c",&ch);
+if((ch>='a'&& ch<='z')||(ch>='A'&&ch<='Z')
+{
+printf("the given input is alphabet");
+}
+else
+{
+printf("the given input is not alphabet");
+}
+}
