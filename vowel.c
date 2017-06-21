38c67084ec4e8ff8235a7a04e43d165b3f9061be
Ramyaamirthalingam <ramya12.eng@gmail.com>
2017-06-21 14:49:17 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch;
+clrscr();
+printf("enter the character");
+printf("%c",ch);
+switch(ch)
+{
+case 'a': Printf("vowel");break;
+case 'b': Printf("vowel");break;
+case 'a': Printf("vowel");break;
+case 'a': Printf("vowel");break;
+case 'a': Printf("vowel");break;
+case 'a': Printf("vowel");break;
+case 'a': Printf("vowel");break;
+case 'a': Printf("vowel");break;
+default : printf("consonants");
+}
+getch();
+}
