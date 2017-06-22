8970764cf8b182d9456ee571dea37f6f48a9834a
Ramyaamirthalingam <ramya12.eng@gmail.com>
2017-06-22 11:22:48 +0530

    Create biggest.c

---
+++ biggest.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("enter value for a,b and c");
+Scanf("%d%d%d",&a,&b,&c);
+if(a>b && a>c)
+{
+Printf(" tha number a %d is bigger",a);
+}
+elseif(b>a && b>c)
+{
+Printf(" tha number b %d is bigger",b);
+}
+else
+{
+Printf(" tha number c %d is bigger",c);
+}
+}
